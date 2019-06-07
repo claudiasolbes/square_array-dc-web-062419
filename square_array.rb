9a1509874fdb3920c7f927fb7fd6437b94393992
Claudia Solbes <cssolbes@hotmail.com>
2019-06-07 00:57:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-  numbers = ["1", "2", "3"]
-  numbers.each do |number|
-    puts numbers ** 2
+  array = ["1", "2", "3"]
+  array.each do |number|
+    puts array ** 2
 end
